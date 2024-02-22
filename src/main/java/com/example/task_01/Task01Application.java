95a20065012ffeb8cb5e2068205885c13abe9c5a
Aebel Shajan <aebelshajan@gmail.com>
2024-02-22 15:09:37 +0000

    Create boilerplate

---
+++ src/main/java/com/example/task_01/Task01Application.java
@@ -0,0 +1,13 @@
+package com.example.task_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_01/Task01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
